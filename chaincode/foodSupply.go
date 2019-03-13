0e5bfa178437c2e767f49a5007decd4d028d4942
Kalonji <kkbankol@us.ibm.com>
2019-03-12 17:13:50 -0700

    adding golang smart contracts

---
+++ chaincode/foodSupply.go
@@ -0,0 +1,276 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+// ============================================================================================================================
+// Product Definitions - The ledger will store marbles and owners
+// ============================================================================================================================
+
+// TODO, add
+// type Status struct{
+//   o INITIALREQUEST
+//   o EXEMPTCHECKREQ
+//   o HAZARDANALYSISCHECKREQ
+//   o CHECKCOMPLETED
+// }
+
+
+// Concept
+type Product struct {
+	// ObjectType string        `json:"docType"` //field for couchdb
+	// productId       string          `json:"productId"`      //the fieldtags are needed to keep case from bouncing around
+  Id       string          `json:"id"`
+	Quantity      string        `json:"quantity"`
+	CountryId       string           `json:"countryId"`
+	// Temperature 			string
+	// Owner      OwnerRelation `json:"owner"`
+}
+
+// Participants
+// TODO, inheriting from User might be unnecessary
+type User struct {
+  firstName string
+  lastName string
+  middleName string
+	Id				string
+  Type      string
+}
+
+type Retailer struct {
+    User
+    Id       string          `json:"id"`
+    Products []string        `json:"products"` // making this a list of product ids
+    // Products []Product        `json:"products"`
+}
+
+type Importer struct {
+    User
+    Id       string          `json:"id"`
+}
+
+type Supplier struct {
+    User
+    Id       string          `json:"id"`
+    countryId       string           `json:"countryid"`
+    orgId       string           `json:"orgId"`
+}
+
+type Regulator struct {
+    Id       string          `json:"id"`
+    countryId       string           `json:"countryid"`
+    ExemptedOrgIds       []string           `json:"exemptedorgids"`
+    ExemptedProductIds       []string           `json:"exemptedproductids"`
+}
+
+// Products
+type ProductListingContract struct {
+    Id       string          `json:"id"` // listingId
+    Status   string           `json:"status"`
+    Products []string        `json:"products"` // making this a list of product ids
+    // Owner    User            `json:"owner"`
+		Owner    string            `json:"owner"`
+    OwnerType    string            `json:"ownertype"` // is this necessary?
+		Supplier string        `json:"supplier"`
+    // Supplier Supplier        `json:"supplier"`
+}
+
+// write functions for transactions
+// transactions?
+
+// transferListing
+// change Owner field in ProductListingContract
+
+
+// // Importer --> Regulator w/o hazard analysis i.e. iniitial check. onSuccess transfer assests to retailer on failure send back to importer for hazard analysis
+// // Importer --> Regulator with hazard analysis. Assumes the Importer has sent the hazard analysis report to Regulator. onSuccess transfer assests to retailer
+// transaction  checkProducts{
+//   --> Regulator regulator
+//   --> ProductListingContract productListing
+// }
+
+
+
+
+
+// // ----- Owners ----- //
+// type Owner struct {
+// 	ObjectType string `json:"docType"`     //field for couchdb
+// 	Id         string `json:"id"`
+// 	Username   string `json:"username"`
+// 	Company    string `json:"company"`
+// 	Enabled    bool   `json:"enabled"`     //disabled owners will not be visible to the application
+// }
+//
+// type OwnerRelation struct {
+// 	Id         string `json:"id"`
+// 	Username   string `json:"username"`    //this is mostly cosmetic/handy, the real relation is by Id not Username
+// 	Company    string `json:"company"`     //this is mostly cosmetic/handy, the real relation is by Id not Company
+// }
+
+// ============================================================================================================================
+// Main
+// ============================================================================================================================
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode - %s", err)
+	}
+}
+
+
+// ============================================================================================================================
+// Init - initialize the chaincode
+//
+// Marbles does not require initialization, so let's run a simple test instead.
+//
+// Shows off PutState() and how to pass an input argument to chaincode.
+// Shows off GetFunctionAndParameters() and GetStringArgs()
+// Shows off GetTxID() to get the transaction ID of the proposal
+//
+// Inputs - Array of strings
+//  ["314"]
+//
+// Returns - shim.Success or error
+// ============================================================================================================================
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("Food Regualation Chaincode Is Starting Up")
+	funcName, args := stub.GetFunctionAndParameters()
+	var number int
+	var err error
+	txId := stub.GetTxID()
+
+	fmt.Println("Init() is running")
+	fmt.Println("Transaction ID:", txId)
+	fmt.Println("  GetFunctionAndParameters() function:", funcName)
+	fmt.Println("  GetFunctionAndParameters() args count:", len(args))
+	fmt.Println("  GetFunctionAndParameters() args found:", args)
+
+	// expecting 1 arg for instantiate or upgrade
+	if len(args) == 1 {
+		fmt.Println("  GetFunctionAndParameters() arg[0] length", len(args[0]))
+
+		// expecting arg[0] to be length 0 for upgrade
+		if len(args[0]) == 0 {
+			fmt.Println("  Uh oh, args[0] is empty...")
+		} else {
+			fmt.Println("  Great news everyone, args[0] is not empty")
+
+			// convert numeric string to integer
+			number, err = strconv.Atoi(args[0])
+			if err != nil {
+				return shim.Error("Expecting a numeric string argument to Init() for instantiate")
+			}
+
+			// this is a very simple test. let's write to the ledger and error out on any errors
+			// it's handy to read this right away to verify network is healthy if it wrote the correct value
+			err = stub.PutState("selftest", []byte(strconv.Itoa(number)))
+			if err != nil {
+				return shim.Error(err.Error())                  //self-test fail
+			}
+		}
+	}
+
+	// showing the alternative argument shim function
+	alt := stub.GetStringArgs()
+	fmt.Println("  GetStringArgs() args count:", len(alt))
+	fmt.Println("  GetStringArgs() args found:", alt)
+
+	// store compatible marbles application version
+	err = stub.PutState("food_reg_ui", []byte("4.0.1"))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("Ready for action")                          //self-test pass
+	return shim.Success(nil)
+}
+
+
+// ============================================================================================================================
+// Invoke - Our entry point for Invocations
+// ============================================================================================================================
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println(" ")
+	fmt.Println("starting invoke, for - " + function)
+
+	// Handle different functions
+	if function == "init" {                    //initialize the chaincode state, used as reset
+		return t.Init(stub)
+	} else if function == "read" {             //generic read ledger
+		return read(stub, args)
+	} else if function == "write" {            //generic writes to ledger
+		return write(stub, args)
+	} else if function == "init_product" {      //create a new marble
+		return init_product(stub, args)
+	} else if function == "init_product_listing" {      //create a new marble
+		return init_product_listing(stub, args)
+	} else if function == "init_user"{        //create a new marble owner
+		return init_user(stub, args)
+  } else if function == "init_regulator" {        //change owner of a marble
+		return init_regulator(stub, args)
+	} else if function == "transfer_product_listing" {        //change owner of a marble
+		return transfer_product_listing(stub, args)
+	} else if function == "check_products" {        //change owner of a marble
+		return check_products(stub, args)
+	} else if function == "read_everything"{   //read everything, (owners + marbles + companies)
+		return read_everything(stub)
+	} else if function == "getHistory"{        //read history of a marble (audit)
+		return getHistory(stub, args)
+  }
+	// } else if function == "getMarblesByRange"{ //read a bunch of marbles by start and stop id
+	// 	return getMarblesByRange(stub, args)
+	// } else if function == "disable_owner"{     //disable a marble owner from appearing on the UI
+	// 	return disable_owner(stub, args)
+
+
+
+  // create product
+  // create user (type required...retailer, importer, or supplier)
+  // create regulator (not inherited from user)
+  // transfer product listing
+  // check products
+  // update exempted list
+
+
+	// error out
+	fmt.Println("Received unknown invoke function name - " + function)
+	return shim.Error("Received unknown invoke function name - '" + function + "'")
+}
+
+
+// ============================================================================================================================
+// Query - legacy function
+// ============================================================================================================================
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Error("Unknown supported call - Query()")
+}
---
+++ chaincode/write_ledger.go
@@ -0,0 +1,452 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+  // "encoding/csv"
+	"fmt"
+	// "strconv"
+	"strings"
+	// "reflect"
+	// "math"
+	// "time"
+	// "encoding/gob"
+	// "bytes"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// ============================================================================================================================
+// write() - genric write variable into ledger
+//
+// Shows Off PutState() - writting a key/value into the ledger
+//
+// Inputs - Array of strings
+//    0   ,    1
+//   key  ,  value
+//  "abc" , "test"
+// ============================================================================================================================
+func write(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var key, value string
+	var err error
+	fmt.Println("starting write")
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2. key of the variable and value to set")
+	}
+
+	// input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	key = args[0]                                   //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value))         //write the variable into the ledger
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end write")
+	return shim.Success(nil)
+}
+
+// ============================================================================================================================
+// delete_marble() - remove a marble from state and from marble index
+//
+// Shows Off DelState() - "removing"" a key/value from the ledger
+//
+// Inputs - Array of strings
+//      0      ,         1
+//     id      ,  authed_by_company
+// "m999999999", "united marbles"
+// ============================================================================================================================
+func delete(stub shim.ChaincodeStubInterface, args []string) (pb.Response) {
+	fmt.Println("starting delete")
+
+	id := args[0]
+
+	err := stub.DelState(id)                                                 //remove the key from chaincode state
+	if err != nil {
+		return shim.Error("Failed to delete state")
+	}
+
+	fmt.Println("- end delete")
+	return shim.Success(nil)
+}
+
+// ============================================================================================================================
+// Init Product - create a new product, store into chaincode state
+//
+// Shows off building a key's JSON value manually
+//
+// Inputs - Array of strings
+
+//      0      ,    1  ,     				2  ,      																			3          			 ,    4     ,     5					, 	6
+//     id      ,  loan amount ,  borrower_info , 															, state								 , interest ,  balance due	, grade
+// "m999999999", "545,000"    ,   object																				deliquent/in payment , 	3.0    , 		520,000			,  BBB
+															// credit/income verification/debt to income,
+
+// ============================================================================================================================
+func init_product(stub shim.ChaincodeStubInterface, args []string) (pb.Response) {
+	var err error
+	fmt.Println("starting init_product")
+	var product Product
+	product.Id = args[0]
+	product.Quantity =  args[1]
+	product.CountryId = args[2]
+	// check if product already exists
+	// TODO, uncomment
+	// _, err = get_product(stub, product.Id)
+	// if err == nil {
+	// 	fmt.Println("This product already exists - " + product.Id)
+	// 	return shim.Error("This product already exists - " + product.Id)
+	// }
+	//store product
+	productAsBytes, _ := json.Marshal(product)                         //convert to array of bytes
+	fmt.Println("writing product to state")
+	fmt.Println(string(productAsBytes))
+	err = stub.PutState(product.Id, productAsBytes)                    //store owner by its Id
+	if err != nil {
+		fmt.Println("Could not store product")
+		return shim.Error(err.Error())
+	}
+	fmt.Println("- end init_product")
+
+	return shim.Success(nil)
+}
+
+
+// update_product
+
+
+// generate_securities
+
+
+
+
+
+
+
+// pool_product (product)
+// underwriting info gives grade
+// grade determines pool
+// pool determines return, but also likliehood of failure/delinquency
+
+
+// ============================================================================================================================
+// Init Owner - create a new owner aka end user, store into chaincode state
+//
+// Shows off building key's value from GoLang Structure
+//
+// Inputs - Array of Strings
+//           0     ,     1   ,   2
+//      owner id   , username, company
+// "o9999999999999",     bob", "united marbles"
+// ============================================================================================================================
+func init_user(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	fmt.Println("starting init_user")
+
+	//input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+  // TODO, should probably allow JSON object as third arg
+	// user.ObjectType = "product_user"
+  id := args[0]
+  userType := args[1]
+
+  var user User
+	user.Id = id
+  user.Type = userType
+
+  // userType := "supplier"
+  // switch user.Type := {
+
+  switch userType {
+    case "supplier":
+      var supplier Supplier
+      supplier.User = user
+      supplier.countryId = args[1]
+      supplier.orgId = args[2]
+      supplierAsBytes, _ := json.Marshal(supplier)                         //convert to array of bytes
+    	err = stub.PutState(id, supplierAsBytes)                    //store owner by its Id
+    	if err != nil {
+    		fmt.Println("Could not store supplier")
+    		return shim.Error(err.Error())
+    	}
+    case "importer":
+      var importer Importer
+      importer.User = user
+      importerAsBytes, _ := json.Marshal(importer)                         //convert to array of bytes
+      err = stub.PutState(id, importerAsBytes)                    //store owner by its Id
+    	if err != nil {
+    		fmt.Println("Could not store importer")
+    		return shim.Error(err.Error())
+    	}
+    case "retailer":
+      var retailer Retailer
+      retailer.User = user
+      retailer.Products = nil //[]Product
+      retailerAsBytes, _ := json.Marshal(retailer)                         //convert to array of bytes
+      err = stub.PutState(id, retailerAsBytes)                    //store owner by its Id
+    	if err != nil {
+    		fmt.Println("Could not store retailer")
+    		return shim.Error(err.Error())
+    	}
+  }
+	fmt.Println("- end init_user")
+	return shim.Success(nil)
+}
+
+
+
+func init_product_listing(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	fmt.Println("starting init_product_listing")
+
+	//input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	// user.ObjectType = "product_user"
+  product_listing_id := args[0]
+  supplier_id := args[1]
+  // product_ids := args[2] // Expecting JSON array of product ids. TODO, csv would probably be easier?
+
+  productListing := ProductListingContract{}
+  productListing.Id = product_listing_id
+  productListing.Status = "INITIALREQUEST"
+  productListing.Owner = supplier_id
+  productListing.Supplier = supplier_id
+  productListing.OwnerType = "Supplier"
+
+  numProducts := len(args) - 2
+  var products = make([]string, numProducts)
+  // all array elements after first 2 are parsed as product ids
+  for i := 2; i < len(args); i++ {
+    products[i - 2] = args[i]
+  }
+  productListing.Products = products //csv.NewReader(product_ids) //json.Unmarshal(product_ids)
+
+  // TODO? update product location to same as supplier
+  // supplierAsBytes, err := stub.GetState(supplier_id)
+  // supplier := Supplier{}
+	// err = json.Unmarshal(supplierAsBytes, &supplier)           //un stringify it aka JSON.parse()
+	// if err != nil {
+	// 	return shim.Error("Error loading supplier")
+	// }
+  productListingAsBytes, _ := json.Marshal(productListing)                         //convert to array of bytes
+  err = stub.PutState(product_listing_id, productListingAsBytes)                    //store owner by its Id
+	if err != nil {
+		fmt.Println("Could not store product listing")
+		return shim.Error(err.Error())
+	}
+	fmt.Println("- end init_product_listing")
+	return shim.Success(nil)
+}
+
+func init_regulator(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	fmt.Println("starting init_regulator")
+
+	//input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+  // regulator_id := args[0]
+  regulator := Regulator{}
+  regulator.Id = args[0]
+  regulator.countryId = args[1]
+  regulatorAsBytes, _ := json.Marshal(regulator)                         //convert to array of bytes
+  err = stub.PutState(regulator.Id, regulatorAsBytes)                    //store owner by its Id
+	if err != nil {
+		fmt.Println("Could not store regulator")
+		return shim.Error(err.Error())
+	}
+	fmt.Println("- end init_regulator")
+	return shim.Success(nil)
+}
+
+func transfer_product_listing(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+  var err error
+	fmt.Println("-starting transfer_product_listing")
+  //input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+  product_listing_id := args[0]
+  new_owner_id := args[1]
+  // user_type := args[1]
+  // user_id := args[1]
+  // retailer_id := args[2]
+
+  productListingAsBytes, err := stub.GetState(product_listing_id)
+  productListing := ProductListingContract{}
+	err = json.Unmarshal(productListingAsBytes, &productListing)           //un stringify it aka JSON.parse()
+	if err != nil {
+		fmt.Println(string(productListingAsBytes))
+		return shim.Error(err.Error())
+	}
+  productListing.Owner = new_owner_id
+  if (strings.ToLower(productListing.OwnerType) == "supplier") {
+    productListing.OwnerType = "Importer"
+    productListing.Status = "EXEMPTCHECKREQ"
+    productListing.Owner = new_owner_id
+
+  } else if ( strings.ToLower(productListing.OwnerType) == "importer" ) {
+    productListing.OwnerType = "Retailer"
+    if ( productListing.Status == "EXEMPTCHECKREQ" ){
+      return shim.Error("Products in listing need to be checked by regulator.")
+    } else if ( productListing.Status == "HAZARDANALYSISCHECKREQ" ){
+      return shim.Error("Products cannot be transferred as they've been flagged by regulator.")
+    }
+    retailerAsBytes, err := stub.GetState(new_owner_id)
+    retailer := Retailer{}
+  	err = json.Unmarshal(retailerAsBytes, &retailer)           //un stringify it aka JSON.parse()
+  	if err != nil {
+  		fmt.Println(string(retailerAsBytes))
+  		return shim.Error(err.Error())
+  	}
+    // _, products := json.Marshal(productListing.Products)
+    for _, product := range productListing.Products {
+      retailer.Products = append(retailer.Products, product)
+    }
+    retailerAsBytes, _ = json.Marshal(retailer)           //convert to array of bytes
+    err = stub.PutState(new_owner_id, retailerAsBytes)     //rewrite the marble with id as key
+    if err != nil {
+      return shim.Error(err.Error())
+    }
+  } else {
+      return shim.Error("Invalid user type provided.")
+  }
+  productListingAsBytes, _ = json.Marshal(productListing)                         //convert to array of bytes
+  err = stub.PutState(product_listing_id, productListingAsBytes)                    //store owner by its Id
+	if err != nil {
+		fmt.Println("Could not store product listing")
+		return shim.Error(err.Error())
+	}
+  fmt.Println("- end transfer_product_listing")
+	return shim.Success(nil)
+}
+
+func update_exempted_list(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+  fmt.Println("- start update_exempted_list")
+  // ["regulator1", "org", "org1", "org2"..]
+  // add list of exempted orgs or products to regulator
+  regulator_id := args[0]
+  exempted_type := args[1]
+  // all remaining args are list of ids
+
+
+  regulatorAsBytes, err := stub.GetState(regulator_id)
+  regulator := Regulator{}
+	err = json.Unmarshal(regulatorAsBytes, &regulator)           //un stringify it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+  numIds := len(args) - 2
+  ids := make([]string, numIds)
+  // all array elements after first 2 are parsed as product ids
+  for i := 2; i < len(args); i++ {
+    ids[i - 2] = args[i]
+  }
+
+  // TODO, should probably append list
+  if (exempted_type == "org") {
+    regulator.ExemptedOrgIds = ids
+  } else if (exempted_type == "product") {
+    regulator.ExemptedProductIds = ids
+  }
+
+  regulatorAsBytes, _ = json.Marshal(regulator)                         //convert to array of bytes
+  err = stub.PutState(regulator_id, regulatorAsBytes)                    //store owner by its Id
+	if err != nil {
+		fmt.Println("Could not store regulator")
+		return shim.Error(err.Error())
+	}
+  fmt.Println("- end update_exempted_list")
+	return shim.Success(nil)
+
+}
+
+func check_products(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+  product_listing_id := args[0]
+  regulator_id := args[1]
+
+  productListingAsBytes, err := stub.GetState(product_listing_id)
+  productListing := ProductListingContract{}
+	err = json.Unmarshal(productListingAsBytes, &productListing)           //un stringify it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+  supplierAsBytes, err := stub.GetState(productListing.Supplier)
+  supplier := Supplier{}
+	err = json.Unmarshal(supplierAsBytes, &supplier)           //un stringify it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+  regulatorAsBytes, err := stub.GetState(regulator_id)
+  regulator := Regulator{}
+	err = json.Unmarshal(regulatorAsBytes, &regulator)           //un stringify it aka JSON.parse()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+  if (productListing.Status != "EXEMPTCHECKREQ" && productListing.Status != "HAZARDANALYSISCHECKREQ"){
+    return shim.Error("Invalid state, listing cannot be checked");
+  }
+
+  check := true
+  if (productListing.Status=="EXEMPTCHECKREQ"){
+    // check if the supplier org is exempted by regulator
+
+    for _, orgId := range regulator.ExemptedOrgIds {
+      if ( supplier.orgId == orgId ) {
+        check = false
+      }
+    }
+  }
+
+  if (check) {
+    productListing.Status="CHECKCOMPLETED"
+  } else {
+    productListing.Status="HAZARDANALYSISCHECKREQ"
+  }
+
+  productListingAsBytes, _ = json.Marshal(productListing)           //convert to array of bytes
+  err = stub.PutState(product_listing_id, productListingAsBytes)     //rewrite the marble with id as key
+  if err != nil {
+    return shim.Error(err.Error())
+  }
+  fmt.Println("- end check_products")
+	return shim.Success(nil)
+}
